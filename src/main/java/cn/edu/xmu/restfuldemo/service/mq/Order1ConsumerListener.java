4fb0bf21a2907595a9480477ea539e58f4499731
leipipi <1195389634@qq.com>
2021-11-02 16:48:05 +0800

    清空data。sql

--- src/main/java/cn/edu/xmu/restfuldemo/service/mq/Order1ConsumerListener.java
+++ src/main/java/cn/edu/xmu/restfuldemo/service/mq/Order1ConsumerListener.java
@@ -1,8 +1,10 @@
 package cn.edu.xmu.restfuldemo.service.mq;
 
 import cn.edu.xmu.restfuldemo.bean.Post_Orders;
+import cn.edu.xmu.restfuldemo.mapper.OrderMapper;
 import cn.edu.xmu.restfuldemo.util.JacksonUtil;
 //import org.apache.commons.text.StringEscapeUtils;
+import com.mysql.cj.x.protobuf.MysqlxCrud;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
@@ -12,16 +14,20 @@ import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.apache.rocketmq.spring.core.RocketMQPushConsumerLifecycleListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @RocketMQMessageListener(topic = "order-topic1", selectorExpression = "1", consumeMode = ConsumeMode.CONCURRENTLY, consumeThreadMax = 10, consumerGroup = "order1-group")
 public class Order1ConsumerListener implements RocketMQListener<String>, RocketMQPushConsumerLifecycleListener {
+    @Autowired
+    OrderMapper orderMapper;
+
     @Override
     public void onMessage(String message) {
         Post_Orders post_orders = JacksonUtil.toObj(message, Post_Orders.class);
-        //OrderMapper.createOrders(post_orders);
+        //orderMapper.createPostOrders(post_orders);
     }
 
     @Override
